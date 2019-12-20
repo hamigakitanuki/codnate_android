f80877a607a136991ba16ded201c2099c56946e5
はみがきたぬき <kakizaki0213@gmail.com>
2019-12-20 13:03:29 +0900

    a

--- app/src/main/java/com/example/codnate3/AWS_INTERFACE.java
+++ app/src/main/java/com/example/codnate3/AWS_INTERFACE.java
@@ -1,7 +1,7 @@
 package com.example.codnate3;
 
 public class AWS_INTERFACE {
-    public static String IPADDRESS = "3.16.214.118:8080";
+    public static String IPADDRESS = "3.16.214.118";
     public static final String IPADDRESS_AI = "3.18.223.66:8080";
 
 }
